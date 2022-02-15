14d30c8268365e3062982c112145111b0b799bc6
unknown <zarjik921@yandex.ru>
2022-02-15 21:52:13 +0300

    правки

--- index.php
+++ index.php
@@ -3,65 +3,25 @@ $is_auth = rand(0, 1);
 
 $user_name = ''; // укажите здесь ваше имя
 
-// Категории
-$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
 
-$items = [
-    [
-        'name' => '2014 Rossingdol District Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => 10900,
-        'image' => 'img/lot-1.jpg'
-    ],
-    [
-        'name' => 'DC Ply Mens 2016/2017 Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => 159999,
-        'image' => 'img/lot-2.jpg'
-    ],
-    [
-        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'category' => 'Крепления',
-        'price' => 8000,
-        'image' => 'img/lot-3.jpg'
-    ],
-    [
-        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'category' => 'Ботинки',
-        'price' => 10999,
-        'image' => 'img/lot-4.jpg'
-    ],
-    [
-        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'category' => 'Одежда',
-        'price' => 7500,
-        'image' => 'img/lot-5.jpg'
-    ],
-    [
-        'name' => 'Маска Oakley Canopy',
-        'category' => 'Разное',
-        'price' => 5400,
-        'image' => 'img/lot-6.jpg'
-    ],
-];
-
-function parametr_sum(int $number_price):string
+function format_sum(int $number):string
 {
     $decimals = 0;
     $dec_point = ".";
     $thousands_sep = " ";
 
     $result = number_format(
-        ceil($number_price),
+        ceil($number),
         $decimals,
-        $dec_points,
+        $dec_point,
         $thousands_sep
     );
 
     return $result . ' <b class="rub"></b>';
 }
-
 ?>
+
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -98,11 +58,10 @@ function parametr_sum(int $number_price):string
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <?php foreach ($categories as $categorie): ?>
-          <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?= $categorie ?></a>
+            <!--заполните этот список из массива категорий-->
+            <li class="promo__item promo__item--boards">
+                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
             </li>
-            <?php endforeach; ?>
         </ul>
     </section>
     <section class="lots">
@@ -110,18 +69,18 @@ function parametr_sum(int $number_price):string
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?php foreach ($items as $item): ?>
+            <!--заполните этот список из массива с товарами-->
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $item['image'] ?>" width="350" height="260" alt="">
+                    <img src="" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $item['category'] ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['name'] ?></a></h3>
+                    <span class="lot__category">Название категории</span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=parametr_sum($item['price']);?></span>
+                            <span class="lot__cost"><?=format_sum($lot['price']);?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -129,7 +88,6 @@ function parametr_sum(int $number_price):string
                     </div>
                 </div>
             </li>
-             <?php endforeach;?>
         </ul>
     </section>
 </main>
@@ -138,11 +96,10 @@ function parametr_sum(int $number_price):string
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <?php foreach ($categories as $categorie): ?>
+            <!--заполните этот список из массива категорий-->
             <li class="nav__item">
-                <a href="pages/all-lots.html"><?= $categorie ?></a>
+                <a href="pages/all-lots.html">Название категории</a>
             </li>
-               <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
