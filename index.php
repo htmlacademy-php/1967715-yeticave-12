5c165932e693d8891bdcb78050e6fb68beec3a7c
zarjik-web <96332372+zarjik-web@users.noreply.github.com>
2022-02-16 22:26:58 +0300

    Правки

--- index.php
+++ index.php
@@ -132,6 +132,7 @@ function parametr_sum(int $number_price):string
                     <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['name'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
+                            <span class="lot__amount">Стартовая цена</span>
                             <span class="lot__cost"><?=parametr_sum($item['price']);?></span>
                         </div>
                         <div class="lot__timer timer">
